11fbab2b320c742d2290e98edf63781f2cfaa9e4
jonasnys@gmail.com <jonasnys@gmail.com>
2023-01-12 20:35:54 +0100

    added GraphicItems::bbox

--- src/lib.rs
+++ src/lib.rs
@@ -33,8 +33,8 @@ impl PathSegments {
 
 #[derive(Debug)]
 pub enum GraphicItem {
-    Line(i32, i32, i32, i32, Stroke),
-    Rect(i32, i32, i32, i32, Stroke, Fill),
+    Line(f32, f32, f32, f32, Stroke),
+    Rect(f32, f32, f32, f32, Stroke, Fill),
     Path(PathSegments, Stroke, Fill),
 }
 
@@ -51,6 +51,81 @@ pub enum Fill {
 }
 
 pub struct GraphicItems(Vec<GraphicItem>);
+
+impl GraphicItems {
+    fn bbox(&self) {
+        let mut x_min = f32::MAX;
+        let mut y_min = f32::MAX;
+        let mut x_max = f32::MIN;
+        let mut y_max = f32::MIN;
+
+        for item in self.0.iter() {
+            match item {
+                GraphicItem::Line(x1, y1, x2, y2, _) => {
+                    x_min = x_min.min(*x1);
+                    y_min = y_min.min(*y1);
+                    x_max = x_max.max(*x1);
+                    y_max = y_max.max(*y1);
+                    x_min = x_min.min(*x2);
+                    y_min = y_min.min(*y2);
+                    x_max = x_max.max(*x2);
+                    y_max = y_max.max(*y2);
+                }
+                GraphicItem::Rect(x, y, w, h, _, _) => {
+                    x_min = x_min.min(*x);
+                    y_min = y_min.min(*y);
+                    x_max = x_max.max(*x);
+                    y_max = y_max.max(*y);
+                    x_min = x_min.min(*x + *w);
+                    y_min = y_min.min(*y + *h);
+                    x_max = x_max.max(*x + *w);
+                    y_max = y_max.max(*y + *h);
+                }
+                GraphicItem::Path(path, _, _) => {
+                    for segment in path.0.iter() {
+                        match segment {
+                            PathSegment::M(x, y) | PathSegment::L(x, y) => {
+                                x_min = x_min.min(*x);
+                                y_min = y_min.min(*y);
+                                x_max = x_max.max(*x);
+                                y_max = y_max.max(*y);
+                            }
+                            PathSegment::Q(x1, y1, x, y) => {
+                                x_min = x_min.min(*x);
+                                y_min = y_min.min(*y);
+                                x_max = x_max.max(*x);
+                                y_max = y_max.max(*y);
+                                x_min = x_min.min(*x1);
+                                y_min = y_min.min(*y1);
+                                x_max = x_max.max(*x1);
+                                y_max = y_max.max(*y1);
+                            }
+                            PathSegment::C(x1, y1, x2, y2, x, y) => {
+                                x_min = x_min.min(*x);
+                                y_min = y_min.min(*y);
+                                x_max = x_max.max(*x);
+                                y_max = y_max.max(*y);
+                                x_min = x_min.min(*x1);
+                                y_min = y_min.min(*y1);
+                                x_max = x_max.max(*x1);
+                                y_max = y_max.max(*y1);
+                                x_min = x_min.min(*x2);
+                                y_min = y_min.min(*y2);
+                                x_max = x_max.max(*x2);
+                                y_max = y_max.max(*y2);
+                            }
+                            PathSegment::Z => {}
+                        }
+                    }
+                }
+            }
+        }
+        println!(
+            "x_min:{}, y_min:{}, x_max:{}, y_max:{}",
+            x_min, y_min, x_max, y_max
+        );
+    }
+}
 
 pub trait GraphicBuilder {
     fn build(&mut self, items: GraphicItems);
@@ -151,13 +226,16 @@ mod tests {
     };
 
     fn testdata() -> GraphicItems {
-        let line = Line(10, 10, 100, 10, Strokestyle(3, 0));
+        let line = Line(10., 10., 100., 10., Strokestyle(3, 0));
         let path = Path(
-            PathSegments(vec![M(0.0, 0.0), L(10.0, 10.0)]),
+            PathSegments(vec![M(5.0, 2.0), L(10.0, 10.0)]),
             NoStroke,
             NoFill,
         );
-        let items = GraphicItems(vec![line, path]);
+        let items = GraphicItems(vec![
+            // line,
+            path,
+        ]);
         items
     }
 
@@ -170,6 +248,12 @@ mod tests {
     fn test_line() {
         let items = testdata();
         assert_eq!(items.0.len(), 1);
+    }
+
+    #[test]
+    fn test_bbox() {
+        let items = testdata();
+        let bbox = items.bbox();
     }
 
     #[test]
