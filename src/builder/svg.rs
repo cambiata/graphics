225227c1879eaf22e498cd72f30617d62bc5505d
jonasnys@gmail.com <jonasnys@gmail.com>
2023-07-14 01:55:48 +0200

    added svg namespace

--- src/builder/svg.rs
+++ src/builder/svg.rs
@@ -35,6 +35,9 @@ impl GraphicBuilder for SvgBuilder {
 
         let mut svg = xmlwriter::XmlWriter::new(xmlwriter::Options::default());
         svg.start_element("svg");
+        svg.write_attribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.write_attribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
+
         svg.write_attribute_fmt(
             "viewBox",
             format_args!(
